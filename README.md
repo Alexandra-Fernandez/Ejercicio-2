7e78122c50b38cceb4c72dd5c519a436ac8e664b
Alexandra <alexandrafernandezquispe@gmail.com>
2016-08-25 15:44:55 -0500

    ejercicio-2

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # Ejercicio-2
+Calcula el promedio de tres números
+
+1. INICIO
+2. PEDIR DATOS
+   - dato 1
+   - dato 2
+   - dato 3 
+3. PROMEDIAR DATOS
+4. ENTREGAR RESULTADO
+5. FIN
+
+[![20160825_144202.jpg](https://s12.postimg.org/b03w3j6al/20160825_144202.jpg)](https://postimg.org/image/rb3zzuis9/)
---
+++ app.js
@@ -0,0 +1,6 @@
+var primerNumero = prompt("primer numero a promediar","");
+var segundoNumero = prompt("segundo numero a promediar","");
+var tercerNumero = prompt("tercer numero a promediar")
+var promedio = (parseInt(primerNumero) + parseInt(segundoNumero) + parseInt(tercerNumero)) / 3 ;
+window.alert("el promedio es igual a");
+window.alert(promedio);
